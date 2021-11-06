4a86bd04c4c4c36bffa75bc64d2e93e6f10d4522
jcc <jcc@Jeans-Air.fios-router.home>
2021-11-06 10:40:33 -0400

    change 4 commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['660366912873.dkr.ecr.us-east-1.amazonaws.com','0.0.0.0','localhost','127.0.0.1','ec2-35-174-77-175.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1','ec2-35-174-77-175.compute-1.amazonaws.com','35.174.77.175']
 
 
 # Application definition
